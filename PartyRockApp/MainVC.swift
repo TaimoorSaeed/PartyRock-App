ccdabfc0a8fe7d82260b269eb0e52669ccfdf207
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-01-18 17:08:30 +0500

    basic structure

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Nano Degree on 18/01/2017.
+//  Copyright © 2017 Swift Pakistan. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
