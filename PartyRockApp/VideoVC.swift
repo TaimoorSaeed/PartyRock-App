d60713a52b2829c72cf0aeb43c440eb186db1ee5
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-01-18 20:19:43 +0500

    ui-completed

---
+++ PartyRockApp/VideoVC.swift
@@ -0,0 +1,24 @@
+//
+//  VideoVC.swift
+//  PartyRockApp
+//
+//  Created by Nano Degree on 18/01/2017.
+//  Copyright © 2017 Swift Pakistan. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
